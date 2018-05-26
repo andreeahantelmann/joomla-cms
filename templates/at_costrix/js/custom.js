044c1451a1fac361742bf8dbe636353a75b2ada3
andreeahantelmann <39635041+andreeahantelmann@users.noreply.github.com>
2018-05-26 14:06:48 +0300

    at_costrix - Default
    Default for all pages

---
+++ templates/at_costrix/js/custom.js
@@ -0,0 +1,37 @@
+/* 
+Counter Up
+*/
+jQuery(function($){
+    $('.counter').counterUp({
+        delay: 100,
+        time: 800
+    });
+});
+
+/*
+WOW plugin triggers animate.css on scroll
+*/
+var wow = new WOW(
+        {
+            boxClass: 'wow', // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset: 100, // distance to the element when triggering the animation (default is 0)
+            mobile: false        // trigger animations on mobile devices (true is default)
+        }
+);
+wow.init();
+
+
+jQuery(function($){
+
+  $('.at-contact-us-map').hover(function() {
+    $('.at-contact-us-title').css('top', '-200px');
+    $('.at-contact-us-title').css('opacity', '0');
+  }, function() {
+    $('.at-contact-us-title').css('top', '0');
+    $('.at-contact-us-title').css('opacity', '1');
+  });
+
+});
+
+
